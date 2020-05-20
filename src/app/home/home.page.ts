27459fb8abb8ea596b0839561257abb688b94a1c
Miguel Valero <miguel.valero@upc.edu>
2020-05-20 17:24:31 +0200

    Notificaciones

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -15,7 +16,10 @@ export class HomePage {
     this.localNotifications.schedule({
           id: 1,
           text: 'Single Local Notification',
-          data: { secret: 'secret' }
+          data: { secret: 'secret' },
+          smallIcon: 'assets/icon/trans.png',
+          color: '#00FF00',
+          led: { color: '#FF00FF', on: 500, off: 500 },
         });
       }
 }
